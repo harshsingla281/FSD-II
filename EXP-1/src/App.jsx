aebef6df825ba5103d4035b9a65897ffe9e83c8a
Abhinav <iamabhiinavv11@gmail.com>
2026-01-16 10:37:33 +0530

    Added EXP-1 and updated EXP-0 configuration

---
+++ EXP-1/src/App.jsx
@@ -0,0 +1,8 @@
+import Spa from "./components/Spa";
+
+function App() {
+  return <Spa />;
+}
+
+export default App;
+
--- EXP-1/src/components/App.jsx
+++ EXP-1/src/components/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SinglePageApp from './components/Spa'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,5 +35,3 @@ function App() {
 }
 
 export default App
-
-
